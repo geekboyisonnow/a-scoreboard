91ceca0e5988e22d9fb18d90d3932f0f2d8daef0
geekboyisonnow <kennethfcannon@gmail.com>
2020-11-14 13:57:30 -0500

    Dynamic Team Button Names / Functions

--- public/main.js
+++ public/main.js
@@ -1,39 +1,97 @@
-let count = 0;
+let countForTeamOne = 0;
+let countForTeamTwo = 0;
 
 const addOnePointForTeamOne = () => {
   console.log('Hurray, we clicked!');
-  count += 1;
+  countForTeamOne += 1;
 
   // Find ptagCount
-  const ptagCount = document.querySelector('p')
-  console.log(ptagCount);
+  const ptagCountForTeamOne = document.querySelector('p.score-for-team-one')
+  console.log(ptagCountForTeamOne);
   
   // Make ptag content value of count
-  ptagCount.textContent = count;
+  ptagCountForTeamOne.textContent = countForTeamOne;
+}
+
+const subtractOnePointFromTeamOne = () => {
+  console.log('Hurray, we clicked!');
+  countForTeamOne -= 1;
+
+  // Find ptagCount
+  const ptagCountForTeamOne = document.querySelector('p.score-for-team-one')
+  console.log(ptagCountForTeamOne);
+  
+  // Make ptag content value of count
+  ptagCountForTeamOne.textContent = countForTeamOne;
+}
+
+const addOnePointForTeamTwo = () => {
+  console.log('Hurray, we clicked!');
+  countForTeamTwo += 1;
+
+  // Find ptagCount
+  const ptagCountForTeamTwo = document.querySelector('p.score-for-team-two')
+  console.log(ptagCountForTeamTwo);
+  
+  // Make ptag content value of count
+  ptagCountForTeamTwo.textContent = countForTeamTwo;
+}
+
+const subtractOnePointFromTeamTwo = () => {
+  console.log('Hurray, we clicked!');
+  countForTeamTwo -= 1;
+
+  // Find ptagCount
+  const ptagCountForTeamTwo = document.querySelector('p.score-for-team-two')
+  console.log(ptagCountForTeamTwo);
+  
+  // Make ptag content value of count
+  ptagCountForTeamTwo.textContent = countForTeamTwo;
 }
 
 function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
 
-  // find the ptag, t
-  const ptagCount = document.querySelector('p');
-  console.log(ptagCount);
+  // Find the ptag for Team 1
+  const ptagCountForTeamOne = document.querySelector('p.score-for-team-one');
+  console.log(ptagCountForTeamOne);
 
-  // make tag value of count
-  ptagCount.textContent = count;
+  // Make tag value of count
+  ptagCountForTeamOne.textContent = countForTeamOne;
 
-  // Find the add 1 button for team 1
+  // Find the ptag for Team 2
+  const ptagCountForTeamTwo = document.querySelector('p.score-for-team-two');
+  console.log(ptagCountForTeamTwo);
+
+  // Make tag value of count
+  ptagCountForTeamTwo.textContent = countForTeamTwo;  
+
+  // Find the add 1 button for Team 1
   const addOneButton = document.querySelector('button.add-for-team-one');
   console.log(addOneButton);
 
-  // When the add button for team 1 is clicked, increment count and update ptag
+  // When the add button for Team 1 is clicked, increment count and update ptag
   addOneButton.addEventListener('click', addOnePointForTeamOne);
 
-  // Find the subtract 1 button for team 1
+  // Find the subtract 1 button for Team 1
+  const subtractOneButton = document.querySelector('button.subtract-from-team-one');
+  console.log(subtractOneButton);
 
+  // When the subtract button for Team 1 is clicked, decrement count and update ptag
+  subtractOneButton.addEventListener('click', subtractOnePointFromTeamOne);
 
+  // Find the add 1 button for Team 2
+  const addOneButtonForTeamTwo = document.querySelector('button.add-for-team-two');
+  console.log(addOneButtonForTeamTwo);
+
+  // When the add button for Team 2 is clicked, increment count and update ptag
+  addOneButtonForTeamTwo.addEventListener('click', addOnePointForTeamTwo);
+
+  // Find the subtract 1 button for Team 1
+  const subtractOneButtonFromTeamTwo = document.querySelector('button.subtract-from-team-two');
+  console.log(subtractOneButtonFromTeamTwo);
+
+  // When the subtract button for Team 1 is clicked, decrement count and update ptag
+  subtractOneButtonFromTeamTwo.addEventListener('click', subtractOnePointFromTeamTwo);
 }
 
 document.addEventListener('DOMContentLoaded', main)
